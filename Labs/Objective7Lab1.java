eafe2214921b993b1fd76471c425ddfefe81169b
Chris Hiles <chrishiles790@gmail.com>
2020-08-22 16:55:58 -0600

    complete objective7lab1

---
+++ Labs/Objective7Lab1.java
@@ -0,0 +1,30 @@
+public class Objective7Lab1 {
+  public static void main(String[] args) {
+    int counter = 10;
+
+    while(counter > 0) {
+      System.out.println(counter);
+      int num1 = counter - 1;
+      System.out.println(num1);
+      int num2 = num1 - 1;
+      System.out.println(num2);
+      int num3 = num2 - 1;
+      System.out.println(num3);
+      int num4 = num3 - 1;
+      System.out.println(num4);
+      int num5 = num4 - 1;
+      System.out.println(num5);
+      int num6 = num5 - 1;
+      System.out.println(num6);
+      int num7 = num6 - 1;
+      System.out.println(num7);
+      int num8 = num7 - 1;
+      System.out.println(num8);
+      int num9 = num8 - 1;
+      System.out.println(num9);
+      int num10 = num9 - 1;
+      break;
+
+    }
+  }
+}

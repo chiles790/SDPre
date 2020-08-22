749a43d599eba2448e442a355b41fdf014baad17
Chris Hiles <chrishiles790@gmail.com>
2020-08-22 17:06:16 -0600

    complete objective7lab2

---
+++ Labs/Objective7Lab2.java
@@ -0,0 +1,50 @@
+public class Objective7Lab2 {
+  public static void main(String [] args) {
+    int counter = 0;
+
+    while (counter <= 20) {
+      System.out.println(counter);
+      int num1 = counter + 1;
+      System.out.println(num1);
+      int num2 = num1 + 1;
+      System.out.println(num2);
+      int num3 = num2 + 1;
+      System.out.println(num3);
+      int num4 = num3 + 1;
+      System.out.println(num4);
+      int num5 = num4 + 1;
+      System.out.println(num5);
+      int num6 = num5 + 1;
+      System.out.println(num6);
+      int num7 = num6 + 1;
+      System.out.println(num7);
+      int num8 = num7 + 1;
+      System.out.println(num8);
+      int num9 = num8 + 1;
+      System.out.println(num9);
+      int num10 = num9 + 1;
+      System.out.println(num10);
+      int num11 = num10 + 1;
+      System.out.println(num11);
+      int num12 = num11 + 1;
+      System.out.println(num12);
+      int num13 = num12 + 1;
+      System.out.println(num13);
+      int num14 = num13 + 1;
+      System.out.println(num14);
+      int num15 = num14 + 1;
+      System.out.println(num15);
+      int num16 = num15 + 1;
+      System.out.println(num16);
+      int num17 = num16 + 1;
+      System.out.println(num17);
+      int num18 = num17 + 1;
+      System.out.println(num18);
+      int num19 = num18 + 1;
+      System.out.println(num19);
+      int num20 = num19 + 1;
+      System.out.println(num20);
+      break;
+    }
+  }
+}

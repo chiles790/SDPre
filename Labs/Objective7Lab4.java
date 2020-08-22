43b822c73c6634ac2c714949a917b3d97a35ad27
Chris Hiles <chrishiles790@gmail.com>
2020-08-22 17:54:55 -0600

    complete objective7java4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,14 @@
+public class Objective7Lab4 {
+  public static void main(String[] args) {
+    int count = 1;
+
+    while(count <= 20) {
+      count = count + 1 ;
+      int sum = count * 10;
+      sum = sum;
+
+      System.out.println(sum);
+
+    }
+  }
+}

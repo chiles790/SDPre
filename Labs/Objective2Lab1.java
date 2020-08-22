41da7a62276877e815c68f568a3103e032a4fdbf
Chris Hiles <chrishiles790@gmail.com>
2020-08-21 18:34:50 -0600

    completeobjective2Lab1

---
+++ Labs/Objective2Lab1.java
@@ -0,0 +1,11 @@
+public class Objective2Lab1 {
+  public static void main(String[] args) {
+    String hello; // declaring the variable named "hello"
+    hello = "Hello World"; // initializing the variable named "hello"
+    System.out.println(hello = "Hello World");
+
+    /*
+    Your solution goes here
+    */
+  }
+}
---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,16 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Chris Hiles";
+    int age = 25;
+    String state = "Belgium";
+    System.out.println(name);
+    System.out.println(age);
+    System.out.println(state);
+    /*
+    Name is Chris Hiles
+    Age is 25
+    State of birth is Belgium
+    */
+
+  }
+}

f8ef1e74d8048871c56b04c1be66a0bb3f3ee137
Chris Hiles <chrishiles790@gmail.com>
2020-08-22 11:43:55 -0600

    complete objective3lab1

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,10 @@
+public class Objective3Lab1 {
+  public static void main(String[] args) {
+    int num1 = 18;
+    int num2 = 10;
+    int num3 = 8;
+    int total = num1 + num2 + num3;
+    double average = total / 3;
+    System.out.println(average);
+  }
+}

e6dc6dfaa2b15114c0a6851335658501cf900de7
Chris Hiles <chrishiles790@gmail.com>
2020-08-22 12:13:51 -0600

    complete objective3lab2

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,11 @@
+public class Objective3Lab2 {
+  public static void main(String[] args) {
+    int side1 = 8;
+    int side2 = 6;
+    double num1 = Math.sqrt(8 * 8);
+    double num2 = Math.sqrt(6 * 6);
+    double result = Math.sqrt(8) + Math.sqrt(6);
+    double hypotenuse = result * 2;
+    System.out.println(hypotenuse);
+  }
+}

7ec0e70c8f54e16db5a0fe380d80a79eea234f66
Chris Hiles <chrishiles790@gmail.com>
2020-08-22 12:48:14 -0600

    complete objective3lab3

---
+++ Labs/Objective3Lab3.java
@@ -0,0 +1,15 @@
+public class Objective3Lab3 {
+  public static void main(String[] args) {
+    int num1 = 2;
+    int num2 = 3;
+    int num3 = 5;
+    int random1 = 1 + (int)(Math.random() * 6);
+    int random2 = 1 + (int)(Math.random() * 6);
+    int random3 = 1 + (int)(Math.random() * 6);
+    int total = random1 + random2 + random3;
+    System.out.println(random1);
+    System.out.println(random2);
+    System.out.println(random3);
+    System.out.println(total);
+  }
+}

bc954a8adf328aba8e8ec65d73ec437c8eebd9f9
Chris Hiles <chrishiles790@gmail.com>
2020-08-22 18:17:23 -0600

    complete objective8Lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,14 @@
+public class Objective8Lab1 {
+  public static void main(String[] args) {
+    int count = 1;
+    int counter = 10;
+    while(count < 11) {
+      System.out.println(count);
+      count = count + 1;
+    }
+    while(counter > 0) {
+      System.out.println(counter);
+      counter = counter - 1;
+    }
+  }
+}

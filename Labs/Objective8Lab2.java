432b5ed81d31f86a7bea0870b24e8c8d09e74d45
Chris Hiles <chrishiles790@gmail.com>
2020-08-22 18:31:22 -0600

    complete objective8lab2

---
+++ Labs/Objective8Lab2.java
@@ -0,0 +1,14 @@
+class Objective7Lab4 {
+  public static void main(String[] args) {
+    int count = 1;
+
+    while(count <= 20) {
+      count = count + 1 ;
+      int sum = count * 10;
+      sum = sum;
+
+      System.out.println(sum);
+
+    }
+  }
+}

71f7d151f1a7018cc7e57a8b75aef1bc33832241
Chris Hiles <chrishiles790@gmail.com>
2020-08-23 02:40:31 -0600

    complete objective8lab3

---
+++ Labs/Objective8Lab3.java
@@ -0,0 +1,48 @@
+public class Objective8Lab3 {
+  public static void main(String[] args) {
+    int counter = 1;
+
+    while(counter % 2 <= 20) {
+      System.out.println(counter + " is odd ");
+      int num1 = counter + 1;
+      System.out.println(num1 + " is even ");
+      int num2 = num1 + 1;
+      System.out.println(num2 + " is odd ");
+      int num3 = num2 + 1;
+      System.out.println(num3 + " is even ");
+      int num4 = num3 + 1;
+      System.out.println(num4 + " is odd ");
+      int num5 = num4 + 1;
+      System.out.println(num5 + " is even ");
+      int num6 = num5 + 1;
+      System.out.println(num6 + " is odd ");
+      int num7 = num6 + 1;
+      System.out.println(num7 + " is even ");
+      int num8 = num7 + 1;
+      System.out.println(num8 + " is odd ");
+      int num9 = num8 + 1;
+      System.out.println(num9 + " is even");
+      int num10 = num9 + 1;
+      System.out.println(num10 + " is odd");
+      int num11 = num10 + 1;
+      System.out.println(num11 + " is even ");
+      int num12 = num11 + 1;
+      System.out.println(num12 + " is odd ");
+      int num13 = num12 + 1;
+      System.out.println(num13 + " is even ");
+      int num14 = num13 + 1;
+      System.out.println(num14 + " is odd ");
+      int num15 = num14 + 1;
+      System.out.println(num15 + " is even ");
+      int num16 = num15 + 1;
+      System.out.println(num16 + " is odd ");
+      int num17 = num16 + 1;
+      System.out.println(num17 + " is even ");
+      int num18 = num17 + 1;
+      System.out.println(num18 + " is odd ");
+      int num19 = num18 + 1;
+      System.out.println(num19 + " is even ");
+      break;
+    }
+  }
+}
